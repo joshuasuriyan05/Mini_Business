f62fcde949c841c54c6157b7a3e2a2d541185171
jowhuq <joshuasuriyan@karunya.edu.in>
2026-06-17 15:58:50 +0530

    changed prisma file

--- backend/src/lib/prisma.js
+++ backend/src/lib/prisma.js
@@ -1,7 +1,10 @@
-import "dotenv/config";
-import { PrismaClient } from "../generated/prisma/client.js";
-import { PrismaPg } from "@prisma/adapter-pg";
+require('dotenv').config();
+const { PrismaClient } = require('../generated/prisma');
+const { PrismaPg } = require('@prisma/adapter-pg');
 const adapter = new PrismaPg({
-connectionString: process.env.DATABASE_URL,
+  connectionString: process.env.DATABASE_URL,
 });
-export const prisma = new PrismaClient({ adapter });
+const prisma = new PrismaClient({
+  adapter,
+});
+module.exports = {prisma,};
